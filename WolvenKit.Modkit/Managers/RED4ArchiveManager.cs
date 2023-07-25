7cd6cd6dbb135e2855532ce8f44b45062d181f35
pharingWell <pharingwell@gmail.com>
2023-07-25 02:00:06 -0400

    dfs

--- WolvenKit.Modkit/Managers/RED4ArchiveManager.cs
+++ WolvenKit.Modkit/Managers/RED4ArchiveManager.cs
@@ -8,8 +8,10 @@ namespace WolvenKit.Common
     /// </summary>
     public abstract class RED4ArchiveManager : WolvenKitArchiveManager
     {
-        public RED4ArchiveManager()
+        protected readonly string[] VanillaDlClist = new string[]
         {
-        }
+            "DLC1", "DLC2", "DLC3", "DLC4", "DLC5", "DLC6", "DLC7", "DLC8", "DLC9", "DLC10", "DLC11", "DLC12",
+            "DLC13", "DLC14", "DLC15", "DLC16", "bob", "ep1"
+        };
     }
 }
